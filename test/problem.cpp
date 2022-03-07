acefa9e9b659b46dfee48b5091a8acb0ff5a0ecf
Mikhail Terekhov <Mikhail.A.Terekhov@ya.ru>
2022-03-07 02:50:29 +0100

    Add dependencies

--- test/problem.cpp
+++ test/problem.cpp
@@ -4,6 +4,8 @@
 #include "random.h"
 #include "so3.h"
 
+#include <math.h>
+
 void compute_error( const Eigen::Matrix3d &R, const Eigen::Vector3d &t,
                    const Eigen::Matrix3d &Rsoln, const Eigen::Vector3d &tsoln,
                   double &rot_angle_err, double &trans_angle_err, double &trans_scale_err )
